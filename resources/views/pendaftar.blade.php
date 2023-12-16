953aeafdacbee225dc6e157e28cdf84f28522981
Ilham Syarief <115606564+ilhamsyarief21@users.noreply.github.com>
2023-12-16 22:26:26 +0700

    update

--- resources/views/pendaftar.blade.php
+++ resources/views/pendaftar.blade.php
@@ -4,5 +4,6 @@
 <body class="g-sidenav-show bg-gray-200">
 
     @include('layout.sidebar')
-
-
+<main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
+    @include('layout.table-pengguna')
+</main>
